7d52fa09233d818f308f7206e60566d5d22e55a3
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 11:17:05 +0800

    Create string1.py

---
+++ string1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+
+name = name.strip().title()   # Remove spaces at the beginning and at the end of the string: 
+                              # Converts the first character of each word to upper case                                
+print(f"Hello {name}")
